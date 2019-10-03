8a0911274790f72430a9b4d0a2f347d820ece3ab
ThilakshanArulnesan <th_thilakshan@hotmail.com>
2019-10-03 16:57:55 +0000

    fixed semantics so that opponent moves were registered

--- scripts/opponent.js
+++ scripts/opponent.js
@@ -1,45 +1,56 @@
 class Opponent {
   constructor(playerTiles) {
-    this.activeTiles = playerTiles;
+    this.activeTiles = arrayOf(playerTiles);
     this.hitTiles = undefined;
     this.missTiles = undefined;
     this.difficulty = 1;
+    this.hitTiles = []; //Could be useful for a smarter AI
+    this.missTiles = []; //Could be useful for a smarter AI
+
     //Pop out elements as used
   }
 
-  // checkSquare(a1) {
-  //   let arr = getArr(a1); //A3 ==> (1,3) ==> [1,4]
-  //   arr[1] += 1;
-  //   this.square = 1;
-  // };
+  get numTiles() {
+    return this.activeTiles.length;
+  };
 
   getMove() { //decides on the strategy to use and then makes the move
     if (this.difficulty === 2) {
       return this.pickNearHits();
     }
     return this.pickRandom();
-  }
+  };
 
   pickRandom() { //Level 1 AI
     //Picks a random tile in the array and pops it out:
-    let guessedIndex = Math.floor(Math.random() * (this.activeTiles.length - 1));
+    let guessedIndex = Math.floor(Math.random() * (this.numTiles - 1));
+    let guessedVal = this.activeTiles[guessedIndex]; //This will be the returned value
 
-    let guessedVal = this.activeTiles[guessedIndex].a1(); //This will be the returned value;
-    if (this.activeTiles[guessedIndex].state === "a") {
+    if (this.activeTiles[guessedIndex].state === "a") { //Was it an active tile?
       this.hitTiles.push(this.activeTiles.splice(guessedIndex, 1));
+
+      //I could change the tile state here.
     } else {
       this.missTiles.push(this.activeTiles.splice(guessedIndex, 1));
     }
 
     return guessedVal;
-  }
+  };
 
   pickNearHits() { //Level 2 AI
     return "A1";
-  }
+  };
 
 };
 
+const arrayOf = function(obj) {
+  //takes an object and makes an array
+  let arr = []
+  for (let key in obj) {
+    arr.push(obj[key]);
+  }
+  return arr;
+}
 
 
 
@@ -100,7 +111,6 @@ const placeOpponentShip = function() {
   }
 
   displayTiles();
-  // }
 }
 
 
